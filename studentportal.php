23c8e8f1022d78973f5bb1737cd781763ef54a9f
Anitha R <88485754+Anitha-2002@users.noreply.github.com>
2023-03-18 14:43:42 +0530

    Add files via upload

---
+++ studentportal.php
@@ -0,0 +1,162 @@
+
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+	<link rel="stylesheet" type="text/css" href="css/bootstrap.css">
+	<link rel="stylesheet" type="text/css" href="font-awesome-4.7.0/css/font-awesome.css">
+	<link rel="stylesheet" type="text/css" href="css/style.css">
+	<link rel="stylesheet" type="text/css" href="flickity/flickity.css">
+	<script type="text/javascript" src="flickity/flickity.js"></script>
+	<title>Library Management</title>
+
+</head>
+<body>
+<div class="container">
+	<nav class="navbar navbar-inverse navbar-fixed-top">
+		<div class="container-fluid">
+			<div class="navbar-header">
+				<button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#bs-example">
+					<span class="sr-only">:</span>
+					<span class="icon-bar"></span>
+					<span class="icon-bar"></span>
+					<span class="icon-bar"></span>
+				</button>
+				<a class="navbar-brand" href="#">Library Management System</a>
+			</div>
+
+			<div class="collapse navbar-collapse" id="bs-example">
+				<ul class="nav navbar-nav">
+					<li class="active"><a href="#">Home</a></li>
+					<li><a href="profile.php">View Profile</a></li>
+					<li><a href="borrow-student.php">Borrow Books</a></li>					
+					<li><a href="fine-student.php">Fines</a></li>					
+				</ul>
+				<ul class="nav navbar-nav navbar-right">
+					<li><a href="logout.php">Logout</a></li>
+				</ul>
+			</div>
+		</div>
+	</nav>
+
+</div>
+
+		<div class="container-fluid slide">
+			
+	  		<div class="slider">
+	  			<!-- <h1>Flickity - wrapAround</h1> -->
+
+
+					<div class="carousel" data-flickity='{ "autoPlay": true }'; >
+
+						  <div class="carousel-cell" auto-play >
+						  	<img src="https://saveetha.ac.in/images/sec/2020/home/building_2.jpg">
+						  </div>
+						  <div class="carousel-cell" auto-play>
+						  	<img src="https://images.static-collegedunia.com/public/college_data/images/appImage/1589815633image1.jpg?tr=c-force">
+
+						  </div>
+						  <div class="carousel-cell" auto-play>
+						  	 <img src="https://images.squarespace-cdn.com/content/v1/590ecdedc534a56c75a1aab7/1623386022105-JCDA7T1X9IKJR6061YHB/IMG_0467%2B%25281%2529.jpg?format=1000w">
+						  </div>
+						  
+						  <div class="carousel-cell" auto-play >
+						  	<img src="https://images.squarespace-cdn.com/content/v1/590ecdedc534a56c75a1aab7/1495038455987-8YMCT00IEUPI5OJZAHF2/IMG_0514.JPG?format=1000w">
+						  </div>
+						   <div class="carousel-cell" auto-play>
+						  	<img src="https://images.squarespace-cdn.com/content/v1/590ecdedc534a56c75a1aab7/1495038466550-YJWPH0LBH3C00ZJXO5YJ/IMG_4439.jpg?format=1000w">
+						  </div>
+
+					</div>
+
+					
+
+	  		</div>
+		</div>
+
+		<div class="container slide2">
+			
+	  		<div class="row">
+	  			<div class="col-lg-4 col-md-4 col-sm-4 col-xs-12 column">
+		  			<div class="page-header col-lg-offset-1">
+		  				<h2>Welcome to our portal</h2>
+		  			</div>
+	  				Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+	  				tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+	  				quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+	  				consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
+	  				cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
+	  				proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+
+	  				<a href="#"><p class="slide2"><button class="btn btn-success">Sign Up</button></p></a>
+	  			</div>
+	  			<div class="col-lg-4 col-md-4 col-sm-4 col-xs-12 column">
+		  			<div class="page-header col-lg-offset-1">
+		  				<h2>24/7 Operational Support</h2>
+		  			</div>
+	  				Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+	  				tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+	  				quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+	  				consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
+	  				cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
+	  				proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+	  			</div>
+	  			<div class="col-lg-4 col-md-4 col-sm-4 col-xs-12 column">
+	  				<div class="page-header col-lg-offset-1">
+	  				<h2>Why Us?</h2>
+	  			</div>
+	  				Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+	  				tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+	  				quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+	  				consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
+	  				cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
+	  				proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+	  			</div>
+	  		</div>
+		</div>
+
+		<div class="container-fluid slide3" style="background-color: #282828">
+			<div class="container">
+				<div class="row">
+				<div class="col-lg-3 col-md-3 col-sm-6 col-xs-12">
+					<a href="#" class="thumbnail">
+						<img src="https://images.squarespace-cdn.com/content/v1/590ecdedc534a56c75a1aab7/1495038426651-0R2WP8EG2F9Q28NZBK6D/IMG_0605.JPG?format=1000w">
+					</a>
+				</div>
+				<div class="col-lg-3 col-md-3 col-sm-6 col-xs-12">
+					<a href="#" class="thumbnail">
+						<img src="https://5.imimg.com/data5/GA/RW/GLADMIN-27092136/electrical-and-electronics-engineering-course-250x250.jpg">
+					</a>
+				</div>
+				<div class="col-lg-3 col-md-3 col-sm-6 col-xs-12">
+					<a href="#" class="thumbnail">
+						<img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQdBCpPuqaY97ISPQsX7lb_iwJ_c3Z0Q7yrdQ&usqp=CAU">
+					</a>
+				</div>
+				<div class="col-lg-3 col-md-0 col-sm-6 col-xs-12">
+					<a href="#" class="thumbnail">
+						<img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTeoUGtN8-QRwa5znRlIWt2jjTJQNgfjGqfEg&usqp=CAU">
+					</a>
+				</div>
+			</div>
+			</div>
+			
+		</div>
+		<div class="container_fluid">
+			<p class="page-header center-block">
+				Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+				
+			</p>
+			<p class="page-header center-block">
+				<a href="#"><button class="btn btn-primary">Contact Us</button></a>
+				<a href="http://www.saveetha.ac.in" target="_blank"><button class="btn btn-info">About Us</button></a>
+
+			</p>
+		</div>
+
+
+<script type="text/javascript" src="js/jquery.js"></script>
+<script type="text/javascript" src="js/bootstrap.js"></script>
+</body>
+</html>
